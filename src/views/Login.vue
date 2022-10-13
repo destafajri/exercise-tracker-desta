f58aa9c757a6805c85513b7b1da6cb463c2dac9f
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-13 11:57:25 +0700

    authentication login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,7 @@
     </div>
 
     <!-- Registration -->
-    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+    <form @submit.prevent="login" class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
 
       <!-- Register title -->
       <h1 class="text-3xl text-at-light-green mb-4">Login Here</h1>
@@ -41,18 +41,37 @@
 <!-- Script -->
 <script>
 import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
+
 
 export default {
   name: "login",
   setup() {
     // Create data or vars
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
     const errorMsg = ref(null);
 
     // Login function
-    
-    return { email, password, errorMsg};
+    const login = async () => {
+      try {
+        const { error } = await supabase.auth.signIn({
+          email: email.value,
+          password: password.value,
+        });
+        if (error) throw error;
+        router.push({ name: "Home" });
+      } catch (error) {
+        errorMsg.value = `Error: ${error.message}`;
+        setTimeout(() => {
+          errorMsg.value = null;
+        }, 5000);
+      }
+    };
+
+    return { email, password, errorMsg, login };
   },
 };
 </script>
