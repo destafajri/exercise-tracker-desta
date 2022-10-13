20752f11fea79c45ebe491625d86dd432a32ba5b
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-13 12:31:41 +0700

    login logout state user inteface

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,25 +6,43 @@
         <h1 class="text-lg">Active Tracker</h1>
       </div>
       <ul class="flex flex-1 justify-end gap-x-10">
-        <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
-        <router-link class="cursor-pointer" :to="{ name: '' }">Create</router-link>
-        <router-link class="cursor-pointer" :to="{ name: 'Login' }">Login</router-link>
-        <li class="cursor-pointer">Logout</li>
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }">
+          Home
+        </router-link>
+        <router-link v-if="user" class="cursor-pointer" :to="{ name: '' }">
+          Create
+        </router-link>
+        <router-link v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }">
+          Login
+        </router-link>
+        <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
       </ul>
     </nav>
   </header>
 </template>
 
+<!-- script -->
 <script>
+import store from "../store/index";
+import { computed } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
+
 export default {
   setup() {
     // Get user from store
+    const user = computed(() => store.state.user);
 
     // Setup ref to router
+    const router = useRouter();
 
     // Logout function
+    const logout = async () => {
+      await supabase.auth.signOut();
+      router.push({ name: "Home" });
+    };
 
-    return {};
+    return { logout, user };
   },
 };
 </script>
